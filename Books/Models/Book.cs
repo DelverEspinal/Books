b7e0342ca77603b2ea09e3a962e7863b270667ea
DelverEspinal <delvervarela66@gmail.com>
2025-11-12 13:38:26 -0600

    Initial commit

---
+++ Books/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace Books.Models
+{
+    public class Book
+    {
+        public string titulo;
+        public string autor;
+        public int anio;
+
+       public Book( string p_titulo, string p_autor, int p_anio)
+        { 
+            this.anio = p_anio;
+            this.autor = p_autor;
+            this.titulo = p_titulo;
+
+        }
+    }
+}
